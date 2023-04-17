99cd34a596c1ba302af0006fec97874c9f707876
Rujwal Shrestha <ruzal.sth8888@gmail.com>
2023-04-17 20:57:40 +0545

    api code

---
+++ server.js
@@ -0,0 +1,42 @@
+// server.js
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// Connect to MongoDB database
+mongoose.connect('mongodb://localhost/todo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB!')
+}).catch((error) => {
+  console.error(error)
+})
+
+// Define API endpoints
+app.get('/api/todos', (req, res) => {
+  // TODO: Implement getting all todos
+})
+
+app.get('/api/todos/:id', (req, res) => {
+  // TODO: Implement getting a single todo by ID
+})
+
+app.post('/api/todos', (req, res) => {
+  // TODO: Implement creating a new todo
+})
+
+app.put('/api/todos/:id', (req, res) => {
+  // TODO: Implement updating an existing todo by ID
+})
+
+app.delete('/api/todos/:id', (req, res) => {
+  // TODO: Implement deleting a todo by ID
+})
+
+// Start server
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server running on port ${port}!`)
+})
