89266e300f80b5f53bded507dc575ca2e9b9c4cb
Rujwal Shrestha <ruzal.sth8888@gmail.com>
2023-04-18 10:11:49 +0545

    changed

--- server.js
+++ server.js
@@ -3,15 +3,7 @@ const express = require('express')
 
 const app = express()
 
-// Connect to MongoDB database
-// mongoose.connect('mongodb://localhost/todo', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then(() => {
-//   console.log('Connected to MongoDB!')
-// }).catch((error) => {
-//   console.error(error)
-// })
+
 
 app.get('/', (req, res)=>{
   res.send('Hello world')
